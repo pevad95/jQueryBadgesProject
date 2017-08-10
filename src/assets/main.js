e960934059757a4977e94bd179a9a656116a235d
Vadasz Peter <peter.vadasz@p92rdi.hu>
2017-08-10 14:49:41 +0200

    Create AJAX.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
-  // your code will go here
+  $.ajax('https://www.codeschool.com/users/peter_vadasz.json', {
+    dataType: 'json',
+    success: function(response) {
+        for (badge of response.courses.completed) {
+          var newBadge = $('<div class = "course"></div>');
+          newBadge.append(badge);
+          $("#badge").append(newBadge);
+        }
+    }
+  });
 
 });
